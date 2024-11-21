995bcabb26e6e648dab59dec6d73910d0e6a191e
Jaimeman84 <jaimeman84@gmail.com>
2024-11-20 19:29:32 -0500

    numpy version updates

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jaime\repos\york-classroom-app\venv
+set VIRTUAL_ENV=C:\Users\jaime\repos\classroom-app\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
